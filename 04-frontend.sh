afe5a1351804b6477fdedb5bbe4b6feef3602543
manoj515876 <manojputtaluckey@gmail.com>
2026-02-01 15:28:36 +0530

    frontend configuration script

---
+++ 04-frontend.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nginx -y &>>$LOGS_FILE
+VALIDATE $? "Disabled Nginx"
+
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+VALIDATE $? "Enabled Nginx 1.24 version module"
+
+dnf install nginx -y &>>$LOGS_FILE
+VALIDATE $? "Installed Nginx"
+
+systemctl enable nginx &>>$LOGS_FILE
+VALIDATE $? "Enabled Nginx"
+
+systemctl start nginx &>>$LOGS_FILE
+VALIDATE $? "Started Nginx"
